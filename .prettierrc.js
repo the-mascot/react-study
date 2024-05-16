c61c8ace92b1474ef718e0b447ddb0b32660991d
The-Mascot <dmstn1812@naver.com>
2024-05-16 10:24:37 +0900

    첫 프로젝트 업로드

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    singleQuote: true,
+    bracketSpacing: true,
+    bracketSameLine: false,
+    endOfLine: 'auto',
+    semi: true,
+    useTabs: false,
+    tabWidth: 2,
+    trailingComma: true,
+    printWidth: 120,
+    arrowParens: 'avoid',
+};
